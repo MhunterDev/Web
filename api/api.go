c1a88df2d59f2a91f28431ab9619baf2bde61983
matthew hunter <matthew.hunter@plixer.com>
2023-11-28 20:03:35 +0000

    .

--- api/api.go
+++ api/api.go
@@ -5,13 +5,13 @@ import (
 )
 
 func handelWelcome(c *fiber.Ctx) error {
-	return c.Render("/workspaces/Web/api/html/welcome.html", fiber.Map{}, "html")
+	return c.Render("/workspaces/Web/html/welcome.html", fiber.Map{}, "html")
 }
 
 // Router handles all routes and listens tls
 func Router() {
 	app := fiber.New()
-	app.Static("/static", "./html/css/")
 	app.Get("/", handelWelcome)
+	app.Static("/css/style.css", "./css/style.css", fiber.Static{})
 	app.Listen(":8080")
 }
--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	api "github.com/MhunterDev/Web/api"
-	easy "github.com/MhunterDev/Web/encryption"
 )
 
 func main() {
-
-	easy.BuildFS()
 	api.Router()
 }
